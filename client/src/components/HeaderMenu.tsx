b1a1f8ea61971edeead19fb5806206c6f8fbc4af
marco <toyko2001@gmail.com>
2023-11-30 13:55:34 +0630

    remove other Header

--- client/src/components/HeaderMenu.tsx
+++ client/src/components/HeaderMenu.tsx
@@ -1,7 +1,6 @@
-import { Box, List, ListItem, ListItemText, Menu, MenuItem, styled } from "@mui/material"
+import { Box, List, ListItem, ListItemText, styled } from "@mui/material"
 import { useRef, useState } from "react"
-import { NavLink, useNavigate } from "react-router-dom"
-import ExpandMoreTwoToneIcon from '@mui/icons-material/ExpandMoreTwoTone';
+import { useNavigate } from "react-router-dom"
 
 const ListWrapper = styled(Box)(({theme}) => ({
   ".MuiTouchRipple-root": {
@@ -91,28 +90,8 @@ export function HeaderMenu() {
           >
             <ListItemText primaryTypographyProps={{ noWrap: true }} primary="Sales Categories" />
           </ListItem>
-
-          <ListItem
-            classes={{ root: "MuiListItem-indicators" }}
-            ref={ref}
-            onClick={onClickOpen}
-          >
-            <ListItemText primaryTypographyProps={{ noWrap: true }} primary={
-              <Box display="flex" alignItems="center">
-                Others
-                <Box display="flex" alignItems="center" pl={0.3}>
-                  <ExpandMoreTwoToneIcon fontSize="small" />
-                </Box>
-              </Box>
-            } />
-          </ListItem>
         </List>
       </ListWrapper>
-
-      <Menu anchorEl={ref.current} onClose={onClickClose} open={isOpen}>
-        <MenuItem sx={{ px: 3 }} component={NavLink} to="/auth/login">Login</MenuItem>
-        <MenuItem sx={{ px: 3 }} component={NavLink} to="/auth/register">Register</MenuItem>
-      </Menu>
     </>
   )
 }
