a530d77f630c827540ee99bcd0b5035f703732e0
Koko Lwin <toyko2001@gmail.com>
2023-12-27 10:59:23 +0630

    fixed: bugs for create all of old raw products

---
+++ client/src/components/LoadingTablePlaceholder.tsx
@@ -0,0 +1,20 @@
+import { Box, CircularProgress, styled } from "@mui/material"
+
+
+const MainContent = styled(Box)(() => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  marginTop: 50
+}))
+
+
+export interface LoadingTablePlaceholderProps {
+}
+
+export function LoadingTablePlaceholder(props: LoadingTablePlaceholderProps) {
+  const {} = props
+  return <MainContent>
+    <CircularProgress />
+  </MainContent>
+}
