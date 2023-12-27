13a68148f203d266ef51d2403f9725d156facd37
Koko Lwin <toyko2001@gmail.com>
2023-12-27 15:50:44 +0630

    raw: excel

--- client/src/components/LoadingTablePlaceholder.tsx
+++ client/src/components/LoadingTablePlaceholder.tsx
@@ -15,6 +15,6 @@ export interface LoadingTablePlaceholderProps {
 export function LoadingTablePlaceholder(props: LoadingTablePlaceholderProps) {
   const {} = props
   return <MainContent>
-    <CircularProgress />
+    <CircularProgress disableShrink />
   </MainContent>
 }
