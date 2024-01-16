d07bc64e1761a8017a3c67d792e4629b23c3906f
marco <toyko2001@gmail.com>
2024-01-16 16:07:07 +0630

    init order summary

--- client/src/pages/checkout/OrderSummary.tsx
+++ client/src/pages/checkout/OrderSummary.tsx
@@ -1,9 +1,15 @@
+import { Box, Container, Typography } from "@mui/material"
+
 interface OrderSummaryProps {}
 
 export function OrderSummary(props: OrderSummaryProps) {
   const {} = props
 
   return (
-    <div>OrderSummaryOrderSummary</div>
+    <Container maxWidth="lg">
+      <Box display="flex" flexDirection="column" gap={3}>
+        <Typography variant="h3">Order Summary</Typography>
+      </Box>
+    </Container>
   )
 }
