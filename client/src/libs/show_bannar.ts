4aa1770288d07891f3353d7d3710217f650a33ba
marco <toyko2001@gmail.com>
2024-01-20 11:27:09 +0630

    show bannar

---
+++ client/src/libs/show_bannar.ts
@@ -0,0 +1,28 @@
+// @ts-nocheck
+export default async function show_bannar() {
+  try {
+    const response = await fetch('../../public/bannar.wasm');
+    const wasmBuffer = await response.arrayBuffer();
+    const wasmModule = await WebAssembly.instantiate(wasmBuffer, {});
+    const exports = wasmModule.instance.exports;
+
+    const get_banner = exports.get_bannar;
+    const free_banner = exports.free;
+
+    const getString = (ptr) => {
+      const uint8Memory = new Uint8Array(exports.memory.buffer);
+      const utf8decoder = new TextDecoder('utf-8');
+      const length = uint8Memory.findIndex((element, index) => index === ptr && element === 0) - ptr;
+      const encodedString = uint8Memory.subarray(ptr, ptr + length);
+      return utf8decoder.decode(encodedString);
+    };
+
+    const bannerPtr = get_banner();
+    const banner = getString(bannerPtr);
+
+    console.clear()
+    console.log(banner.split(".com")[0] + ".com");
+
+    free_banner(bannerPtr);
+  } catch (_err) { }
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import show_bannar from './libs/show_bannar.ts'
+
+
+show_bannar()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
