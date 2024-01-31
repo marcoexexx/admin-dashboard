90f2065ad3682cf7a463c81939cb62a4d0a823fb
marco <toyko2001@gmail.com>
2024-01-31 11:02:44 +0630

    add more functions to react type

--- client/src/libs/result/index.ts
+++ client/src/libs/result/index.ts
@@ -1,3 +1,6 @@
+import { Err } from '.'
+import { Ok } from '.'
+
 export * from './ok'
 export * from './err'
 export * from './as_result'
@@ -12,6 +15,12 @@ class UnwrapException extends Error {
   }
 }
 
+class UnreachableException<T extends any> extends Error {
+  constructor(variable: T) {
+    super(`Unreachable: ${{variable}}`)
+  }
+}
+
 export function unwrap_failed<E extends ToString>(msg: string, err: E): never {
   throw new UnwrapException(`${msg}: ${err.toString()}`)
 }
@@ -22,36 +31,105 @@ export default class Result<T, E extends ToString> {
     public value: T | E
   ) {}
 
-  is_ok() {
+  is_ok(): this is Result<T, never> {
     if  (this._type === "ok") return true
     return false
   }
 
-  is_err() {
+  is_err(): this is Result<never, E> {
     if  (this._type === "err") return true
     return false
   }
 
   ok(): T | undefined {
-    if  (this._type === "err") return this.value as T
+    if  (this.is_ok()) return this.value
   }
 
   err(): E | undefined {
-    if  (this._type === "err") return this.value as E
+    if  (this.is_err()) return this.value
   }
 
   expect(msg: string): T {
-    if (this._type === "ok") return this.value as T
-    unwrap_failed(msg, this.value as E)
+    if (this.is_ok()) return this.value
+    else if (this.is_err()) unwrap_failed(msg, this.value)
+    throw new UnreachableException(this.value)
   }
 
   unwrap(): T {
-    if (this._type === "ok") return this.value as T
-    unwrap_failed("called `Result::unwrap()` on an `Err` value", this.value as E)
+    if (this.is_ok()) return this.value
+    else if (this.is_err()) unwrap_failed("called `Result::unwrap()` on an `Err` value", this.value as E)
+    throw new UnreachableException(this.value)
   }
 
+  map<U extends ToString>(op: (x: T) => U): Result<U, E> {
+    if (this.is_ok()) return Ok(op(this.value))
+    else if (this.is_err()) Err(this.value)
+    throw new UnreachableException(this.value)
+  }
+
+  map_or<U>(def: U, f: (x: T) => U): U {
+    if (this.is_ok()) return f(this.value)
+    else if (this.is_err()) def
+    throw new UnreachableException(this.value)
+  }
+
+  map_or_else<U>(def: (err: E) => U, f: (x: T) => U): U {
+    if (this.is_ok()) return f(this.value)
+    else if (this.is_err()) def(this.value)
+    throw new UnreachableException(this.value)
+  }
+
+  map_err<U extends ToString>(op: (err: E) => U): Result<T, U> {
+    if (this.is_ok()) return Ok(this.value)
+    else if (this.is_err()) Err(op(this.value))
+    throw new UnreachableException(this.value)
+  }
+
+  and<U extends ToString>(res: Result<U, E>): Result<U, E> {
+    if (this.is_ok()) return res
+    else if (this.is_err()) Err(this.value)
+    throw new UnreachableException(this.value)
+  }
+
+  and_then<U extends ToString>(op: (x: T) => Result<U, E>): Result<U, E> {
+    if (this.is_ok()) return op(this.value)
+    else if (this.is_err()) Err(this.value)
+    throw new UnreachableException(this.value)
+  }
+
+  or<U extends ToString>(res: Result<T, U>): Result<T, U> {
+    if (this.is_ok()) return Ok(this.value)
+    else if (this.is_err()) res
+    throw new UnreachableException(this.value)
+  }
+
+  or_else<U extends ToString>(op: (err: E) => Result<T, U>): Result<T, U> {
+    if (this.is_ok()) return Ok(this.value)
+    else if (this.is_err()) op(this.value)
+    throw new UnreachableException(this.value)
+  }
+
+  expect_err(msg: string): E {
+    if (this.is_ok()) unwrap_failed(msg, this.value as typeof this.value & ToString)
+    else if (this.is_err()) this.value
+    throw new UnreachableException(this.value)
+  }
+
+  unwrap_err(): E {
+    if (this.is_ok()) unwrap_failed("called `Result::unwrap_err()` on an `Ok` value", this.value as typeof this.value & ToString)
+    else if (this.is_err()) this.value
+    throw new UnreachableException(this.value)
+  }
+
+  unwrap_or(def: T): T {
+    if (this.is_ok()) return this.value
+    else if (this.is_err()) def
+    throw new UnreachableException(this.value)
+  }
+
+
   ok_or_throw(): T {
-    if (this._type === "ok") return this.value as T
-    throw this.value as E
+    if (this.is_ok()) return this.value
+    throw this.value
   }
 }
