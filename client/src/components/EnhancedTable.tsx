1436b43481da803c13a7059f4e8d1b2e30d1d56c
Aung Koko Lwin <toyko2001@gmail.com>
2024-02-21 14:46:58 +0630

    add enhancedtable

---
+++ client/src/components/EnhancedTable.tsx
@@ -0,0 +1,343 @@
+import { Box, CardContent, Checkbox, Divider, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography, useTheme } from "@mui/material";
+import { FormModal } from "./forms";
+import { MuiButton } from "./ui";
+import { BulkActions, EnhancedTableActions, LoadingTablePlaceholder } from ".";
+import { OperationAction, Resource, User } from "@/services/types";
+import { ModalFormField } from "@/context/store";
+import { usePermission, useStore } from "@/hooks";
+import { useMemo, useReducer } from "react";
+import { convertToExcel, exportToExcel } from "@/libs/exportToExcel";
+import { useNavigate } from "react-router-dom";
+
+import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+import AppError, { AppErrorKind } from "@/libs/exceptions";
+
+
+const deleteSingleRowKeys: Record<Resource, ModalFormField> = {
+  [Resource.AccessLog]: "*",
+  [Resource.AuditLog]: "*",
+  [Resource.User]: "*",
+  [Resource.PickupAddress]: "*",
+  [Resource.Brand]: "delete-brand",
+  [Resource.Category]: "delete-category",
+  [Resource.Coupon]: "delete-coupon",
+  [Resource.Exchange]: "delete-exchange",
+  [Resource.Order]: "delete-order",
+  [Resource.PotentialOrder]: "delete-potential-order",
+  [Resource.Product]: "delete-product",
+  [Resource.Region]: "delete-region",
+  [Resource.SalesCategory]: "delete-sales-category",
+  [Resource.Township]: "delete-township",
+  [Resource.UserAddress]: "delete-user-address"
+}
+
+const deleteMultiRowKeys: Record<Resource, ModalFormField> = {
+  [Resource.AccessLog]: "*",
+  [Resource.AuditLog]: "*",
+  [Resource.User]: "*",
+  [Resource.PickupAddress]: "*",
+  [Resource.Brand]: "delete-brand-multi",
+  [Resource.Category]: "delete-category-multi",
+  [Resource.Coupon]: "delete-coupon-multi",
+  [Resource.Exchange]: "delete-exchange-multi",
+  [Resource.Order]: "delete-order-multi",
+  [Resource.PotentialOrder]: "delete-potential-order-multi",
+  [Resource.Product]: "delete-product-multi",
+  [Resource.Region]: "delete-region-multi",
+  [Resource.SalesCategory]: "delete-sales-category-multi",
+  [Resource.Township]: "delete-township-multi",
+  [Resource.UserAddress]: "delete-user-address-multi"
+}
+
+
+export type TypedColumn<T extends object> = {
+  id: keyof T,
+  name: string,
+  align: "right" | "left" | "center",
+  render: ({ value, me }: { value: T, me?: User }) => React.ReactElement | null
+}
+
+export type DynamicColumn<T extends object> = {
+  id: string,
+  name: string,
+  align: "right" | "left" | "center"
+  render: ({ value, me }: { value: T, me?: User }) => React.ReactElement | null
+}
+
+
+const initState = {
+  selectedRows: [] as string[],
+  singleDeleteRow: undefined as string | undefined
+}
+type TableState = typeof initState
+
+type TableAction =
+  | { type: "SET_SELECTED_ROWS", payload: string[] }
+  | { type: "SINGLE_SELECT", payload: string }
+  | { type: "SINGLE_UNSELECT", payload: string }
+  | { type: "SET_DELETE_ROW", payload: string }
+
+
+const reducer = (state: typeof initState, action: TableAction): TableState => {
+  switch (action.type) {
+    case "SET_SELECTED_ROWS": {
+      return { ...state, selectedRows: action.payload }
+    }
+
+    case "SINGLE_SELECT": {
+      return { ...state, selectedRows: [...state.selectedRows, action.payload] }
+    }
+
+    case "SINGLE_UNSELECT": {
+      return { ...state, selectedRows: state.selectedRows.filter(prev => prev !== action.payload) }
+    }
+
+    case "SET_DELETE_ROW": {
+      return { ...state, singleDeleteRow: action.payload }
+    }
+
+    default: {
+      const _unreachable: never = action
+      console.error({ _unreachable })
+      throw AppError.new(AppErrorKind.InvalidInputError, "Unhandled action type")
+    }
+  }
+}
+
+
+interface EnhancedTableProps<Row extends object> {
+  isLoading?: boolean
+  resource: Resource
+  columns: DynamicColumn<Row>[]
+  refreshKey: any
+  rows: Row[]
+  renderFilterForm: React.ReactElement | null
+  hideCheckbox?: boolean
+  hideTopActions?: boolean
+  onSingleDelete?: (id: string) => void
+  onMultiDelete?: (ids: string[]) => void
+  onMultiCreate?: (buf: ArrayBuffer) => void
+}
+
+export function EnhancedTable<Row extends { id: string }>(props: EnhancedTableProps<Row>) {
+  const [state, tableStateDispatch] = useReducer(reducer, initState)
+
+  const { state: { modalForm, user }, dispatch } = useStore()
+  const { isLoading, resource, rows, columns: cols, onSingleDelete, onMultiDelete, onMultiCreate, renderFilterForm, refreshKey, hideCheckbox, hideTopActions } = props
+  const { selectedRows, singleDeleteRow } = state
+
+  const isSelectedAllRows = rows.length === selectedRows.length
+  const isSelectedSomeRows = selectedRows.length > 0 &&
+    selectedRows.length < rows.length
+
+  const navigate = useNavigate()
+  const theme = useTheme()
+  const columns = useMemo(() => cols.concat({
+    id: "actions",
+    name: "Actions",
+    align: "right",
+    render: () => null
+  }), [])
+
+  const isAllowedUpdate = usePermission({
+    action: OperationAction.Update,
+    resource
+  }).is_ok()
+
+  const isAllowedDelete = usePermission({
+    action: OperationAction.Delete,
+    resource
+  }).is_ok()
+
+  const handleSelectAll = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = evt.target
+    tableStateDispatch({ type: "SET_SELECTED_ROWS", payload: checked ? rows.map(i => i.id) : [] })
+  }
+
+  const handleSelectOne = (id: string) => (_: React.ChangeEvent<HTMLInputElement>) => {
+    if (!selectedRows.includes(id)) tableStateDispatch({ type: "SINGLE_SELECT", payload: id })
+    else tableStateDispatch({ type: "SINGLE_UNSELECT", payload: id })
+  }
+
+  const handleClickDeleteAction = (id: string) => (_: React.MouseEvent<HTMLButtonElement>) => {
+    tableStateDispatch({ type: "SET_DELETE_ROW", payload: id })
+    dispatch({ type: "OPEN_MODAL_FORM", payload: deleteSingleRowKeys[resource] })
+  }
+
+  const handleClickUpdateAction = (id: string) => (_: React.MouseEvent<HTMLButtonElement>) => {
+    navigate(`/${refreshKey[0]}/update/${id}`)
+  }
+
+  const handleMultiDelete = (ids: string[]) => () => {
+    if (!onMultiDelete) return console.warn(`Unimplemented multi delete feature: ${resource}`)
+    onMultiDelete(ids)
+  }
+
+  const handleSingleDelete = (id: string) => () => {
+    if (!onSingleDelete) return console.warn(`Unimplemented single delete feature: ${resource}`)
+    onSingleDelete(id)
+  }
+
+  const handleCloseModal = () => {
+    dispatch({ type: "CLOSE_ALL_MODAL_FORM" })
+  }
+
+  const handleOnExport = () => {
+    exportToExcel(rows.filter(row => selectedRows.includes(row.id)), resource)
+  }
+
+  const handleOnImport = (data: Row[]) => {
+    if (!onMultiCreate) return console.warn(`Unimplemented excel import feature: ${resource}`)
+
+    convertToExcel(data, "Brands")
+      .then(excelBuffer => onMultiCreate(excelBuffer))
+      .catch(err => dispatch({
+        type: "OPEN_TOAST",
+        payload: {
+          message: `Failed Excel upload: ${err.message}`,
+          severity: "error"
+        }
+      }))
+  }
+
+
+  return <>
+    {selectedRows.length > 0 && <Box flex={1} p={2}>
+      <BulkActions
+        field={deleteMultiRowKeys[resource]}
+        isAllowedDelete={isAllowedDelete}
+        onDelete={handleMultiDelete(selectedRows)}
+      />
+    </Box>}
+
+    <Divider />
+
+    {hideTopActions
+      ? null
+      : <CardContent>
+        <EnhancedTableActions
+          refreshKey={refreshKey}
+          onImport={handleOnImport}
+          onExport={handleOnExport}
+          renderFilterForm={renderFilterForm}
+          resource={resource}
+        />
+      </CardContent>}
+
+    <TableContainer>
+      {isLoading
+        ? <LoadingTablePlaceholder />
+        : <Table>
+          <TableHead>
+            <TableRow>
+              {hideCheckbox 
+                ? null 
+                : <TableCell padding="checkbox">
+                  <Checkbox
+                    color="primary"
+                    checked={isSelectedAllRows}
+                    indeterminate={isSelectedSomeRows}
+                    onChange={handleSelectAll}
+                  />
+                </TableCell>}
+
+              {columns.map(col => {
+                const render = <TableCell key={col.id} align={col.align}>{col.name}</TableCell>
+                return col.id !== "actions"
+                  ? render
+                  : isAllowedUpdate || isAllowedDelete
+                    ? render
+                    : null
+              })}
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {rows.map(row => {
+              const isSelected = selectedRows.includes(row.id)
+              return <TableRow
+                hover
+                key={row.id}
+                selected={isSelected}
+              >
+                {hideCheckbox
+                  ? null
+                  : <TableCell padding="checkbox">
+                    <Checkbox
+                      color="primary"
+                      checked={isSelected}
+                      onChange={handleSelectOne(row.id)}
+                      value={isSelected}
+                    />
+                  </TableCell>}
+
+                {cols.map((col, idx) => {
+                  return <TableCell key={idx} align={col.align}>
+                    {col.render({ value: row, me: user })}
+                  </TableCell>
+                })}
+
+                {isAllowedUpdate || isAllowedDelete
+                  ? <TableCell align="right">
+                    {isAllowedUpdate
+                      ? <Tooltip title="Edit" arrow>
+                        <IconButton
+                          sx={{
+                            '&:hover': {
+                              background: theme.colors.primary.lighter
+                            },
+                            color: theme.palette.primary.main
+                          }}
+                          color="inherit"
+                          size="small"
+                          onClick={handleClickUpdateAction(row.id)}
+                        >
+                          <EditTwoToneIcon fontSize="small" />
+                        </IconButton>
+                      </Tooltip>
+                      : null}
+
+                    {isAllowedDelete
+                      ? <Tooltip title="Delete" arrow>
+                        <IconButton
+                          sx={{
+                            '&:hover': {
+                              background: theme.colors.error.lighter
+                            },
+                            color: theme.palette.error.main
+                          }}
+                          onClick={handleClickDeleteAction(row.id)}
+                          color="inherit"
+                          size="small"
+                        >
+                          <DeleteTwoToneIcon fontSize="small" />
+                        </IconButton>
+                      </Tooltip>
+                      : null}
+                  </TableCell>
+                  : null}
+              </TableRow>
+            })}
+          </TableBody>
+        </Table>}
+    </TableContainer>
+
+    {modalForm.field === deleteSingleRowKeys[resource] && singleDeleteRow
+      ? <FormModal
+        field={deleteSingleRowKeys[resource]}
+        title="Delete row"
+      >
+        <Box display="flex" flexDirection="column" gap={1}>
+          <Box>
+            <Typography>Are you sure want to delete</Typography>
+          </Box>
+          <Box display="flex" flexDirection="row" gap={1}>
+            <MuiButton variant="contained" color="error" onClick={handleSingleDelete(singleDeleteRow)}>Delete</MuiButton>
+            <MuiButton variant="outlined" onClick={handleCloseModal}>Cancel</MuiButton>
+          </Box>
+        </Box>
+      </FormModal>
+      : null}
+  </>
+}
---
+++ client/src/components/EnhancedTableAction.tsx
@@ -0,0 +1,155 @@
+import { usePermission } from "@/hooks";
+import { useRef, useState } from "react";
+import { queryClient } from "@/components";
+import { Accordion, AccordionDetails, AccordionSummary, Box, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Popover, Tooltip, Typography, styled } from "@mui/material";
+import { OperationAction, Resource } from "@/services/types";
+
+import ExportIcon from '@mui/icons-material/Upgrade';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ImportIcon from '@mui/icons-material/MoveToInbox';
+import RefreshIcon from '@mui/icons-material/Refresh';
+
+import * as XLSX from 'xlsx'
+
+
+const MenuActionBox = styled(Box)(({theme}) => ({
+  background: theme.colors.alpha.black[5],
+  padding: theme.spacing(2)
+}))
+
+
+interface EnhancedTableActionsProps {
+  onExport: () => void
+  onImport: (data: any) => void
+  renderFilterForm: React.ReactElement | null
+  resource: Resource
+  refreshKey: any
+}
+
+export function EnhancedTableActions(props: EnhancedTableActionsProps) {
+  const { onExport, onImport, resource, renderFilterForm, refreshKey } = props
+
+  const ref = useRef<HTMLButtonElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  
+  const [isOpen, setIsOpen] = useState(false)
+
+  const isAllowedImport = usePermission({
+    action: OperationAction.Create,
+    resource
+  }).is_ok()
+
+  const handleOpen = () => {
+    setIsOpen(true)
+  }
+
+  const handleClose = () => {
+    setIsOpen(false)
+  }
+
+  const handleImportExcel = () => {
+    // onImport()
+    inputRef.current?.click()
+  }
+
+  const handleChangeImportExcel = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    const file = (evt.target.files)?.[0]
+    if (!file) return
+    const reader = new FileReader()
+    reader.readAsBinaryString(file)
+    reader.onload = (e) => {
+      // @ts-ignore
+      const data = e.target.result
+      const wb = XLSX.read(data, { type: "binary" })
+      const sheetName = wb.SheetNames[0]
+      const sheet = wb.Sheets[sheetName]
+      const parsedData = XLSX.utils.sheet_to_json(sheet)
+
+      onImport(parsedData)
+    }
+  }
+
+  const handleExportExcel = () => {
+    setIsOpen(false)
+    onExport()
+  }
+
+  const handleRefreshList = () => {
+    queryClient.invalidateQueries({
+      queryKey: refreshKey
+    })
+  }
+
+
+  return (
+    <Box display="flex" justifyContent="space-between" alignItems="baseline" flexDirection="row">
+      <Accordion sx={{ width: "100%" }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel-filter-content"
+          id="panel-filter"
+        >
+          <Typography fontSize={20}>Filter</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          {renderFilterForm}
+        </AccordionDetails>
+      </Accordion>
+
+      <IconButton aria-label="more actions" ref={ref} onClick={handleOpen}>
+        <MoreVertIcon />
+      </IconButton>
+
+      <Tooltip title="Refresh table" arrow>
+        <IconButton aria-label="refresh button" onClick={handleRefreshList}>
+          <RefreshIcon />
+        </IconButton>
+      </Tooltip>
+
+      <Popover
+        anchorEl={ref.current}
+        onClose={handleClose}
+        open={isOpen}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+      >
+        <MenuActionBox display="flex">
+          <List sx={{ p: 1 }} component="div">
+            <ListItemButton onClick={handleExportExcel}>
+              <ListItemIcon>
+                <ExportIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText primary="Export" />
+            </ListItemButton>
+
+            {isAllowedImport
+            ? <ListItemButton onClick={handleImportExcel}>
+                <ListItemIcon>
+                  <ImportIcon fontSize="small" />
+                </ListItemIcon>
+                <input 
+                  type="file"
+                  style={{
+                    display: "none"
+                  }}
+                  ref={inputRef}
+                  accept=".xlsx, .xls"
+                  onChange={handleChangeImportExcel}
+                />
+                <ListItemText primary="Import" />
+              </ListItemButton>
+            : null}
+          </List>
+        </MenuActionBox>
+      </Popover>
+    </Box>
+  )
+}
+
