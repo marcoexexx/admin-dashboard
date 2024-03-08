65c34d956e2a75ea5cd755e99fb5608385d89c9e
Aung Koko Lwin <toyko2001@gmail.com>
2024-03-08 15:57:57 +0630

    add pickup comfirm display

--- client/src/pages/checkout/CheckoutOrderConfirmation/PickupAddressDetailCard.tsx
+++ client/src/pages/checkout/CheckoutOrderConfirmation/PickupAddressDetailCard.tsx
@@ -1,25 +1,36 @@
-// import { CreateOrderInput } from "@/components/content/orders/forms";
-// import { Card, CardContent, Typography } from "@mui/material";
-// import { Dayjs } from "dayjs";
-// import { useFormContext } from "react-hook-form";
+import { SuspenseLoader } from "@/components"
+import { CreateOrderInput } from "@/components/content/orders/forms"
+import { Alert, Card, CardContent, Typography } from "@mui/material"
+import { useGetPickupAddress } from "@/hooks/pickupAddress"
+import { useFormContext } from "react-hook-form"
 
 
 export function PickupAddressDetailCard() {
-  // const { getValues } = useFormContext<CreateOrderInput>()
-  // const { pickupAddressId } = getValues()
+  const { getValues } = useFormContext<CreateOrderInput>()
+  const pickupAddressId = getValues("pickupAddressId")
+
+  const { try_data, isLoading } = useGetPickupAddress({
+    id: pickupAddressId
+  })
+  const pickupAddress = try_data.ok_or_throw()?.pickupAddress
+
+
+  if (isLoading) return <SuspenseLoader />
 
 
   return (
-    <>
-      {/* <Card> */}
-      {/*   <CardContent> */}
-      {/*     <Typography variant="h4">{pickupAddress.username}</Typography> */}
-      {/*     <Typography>Phone: {pickupAddress.phone}</Typography> */}
-      {/*    {pickupAddress.email ? <Typography>Email: {pickupAddress.email}</Typography> : null} */}
-      {/*     <Typography>date: {(pickupAddress.date as Dayjs).toISOString()}</Typography> */}
-      {/*   </CardContent> */}
-      {/* </Card> */}
-      Not available right now!
-    </>
+    <Card>
+      {pickupAddress
+        ? <CardContent>
+          <Typography variant="h4">{pickupAddress.username}</Typography>
+          <Typography>Phone: {pickupAddress.phone}</Typography>
+          {pickupAddress.email ? <Typography>Email: {pickupAddress.email}</Typography> : null}
+          <Typography>date: {(new Date(pickupAddress.date) as Date).toUTCString()}</Typography>
+        </CardContent>
+        : <CardContent>
+          <Alert severity="error">Error: Billing Address Not Found</Alert>
+          <Typography mt={2}>Sorry, we couldn't find the specified billing address. It may have been deleted or does not exist in our records. Please double-check the address and try again. If you continue to experience issues, reach out to our support team for further assistance.</Typography>
+        </CardContent>}
+    </Card>
   )
 }
