1fcd4b7a35c851c8245968c907e8ee97a6a9aa05
Aung Koko Lwin <toyko2001@gmail.com>
2024-03-18 11:22:27 +0630

    fix abstarct service

--- server/src/services/productService/index.ts
+++ server/src/services/productService/index.ts
@@ -1,13 +1,13 @@
 import fs from "fs";
-import AppError, { StatusCode } from "../../utils/appError";
-import Result, { as_result_async, Err, Ok } from "../../utils/result";
+import AppError from "../../utils/appError";
+import Result, { as_result_async, Ok } from "../../utils/result";
 
 import { OperationAction, ProductStatus, Resource, User } from "@prisma/client";
 import { CreateMultiProductsInput } from "../../schemas/product.schema";
 import { convertPrismaErrorToAppError } from "../../utils/convertPrismaErrorToAppError";
 import { db } from "../../utils/db";
 import { parseExcel } from "../../utils/parseExcel";
-import { AppService, MetaAppService, Pagination } from "../type";
+import { AppService } from "../type";
 
 /**
  * ProductService class provides methods for managing product data.
@@ -15,11 +15,12 @@ import { AppService, MetaAppService, Pagination } from "../type";
  * @remarks
  * This class implements the AppService interface and is designed to handle operations related to products.
  */
-export class ProductService extends MetaAppService implements AppService {
-  private repository = db.product;
-
+export class ProductService extends AppService<
+  typeof db.product
+> {
   constructor() {
-    super(Resource.Product, { action: OperationAction.Read, resourceIds: [] });
+    super(Resource.Product, { action: OperationAction.Read, resourceIds: [] }, db.product);
+    this.name = "Product";
   }
 
   /**
@@ -28,151 +29,6 @@ export class ProductService extends MetaAppService implements AppService {
    */
   static new() {
     return new ProductService();
-  }
-
-  async tryCount(): Promise<Result<number, AppError>> {
-    const opt = as_result_async(this.repository.count);
-
-    const result = (await opt()).map_err(convertPrismaErrorToAppError);
-
-    this.log = {
-      action: OperationAction.Read,
-      resourceIds: [],
-    };
-    return result;
-  }
-
-  async tryFindManyWithCount(
-    ...args: [{ pagination: Pagination; }, ...Parameters<typeof this.repository.findMany>]
-  ): Promise<
-    Result<[number, Awaited<ReturnType<typeof this.repository.findMany>>], AppError>
-  > {
-    const [{ pagination }, arg] = args;
-    const { page = 1, pageSize = 10 } = pagination;
-    const offset = (page - 1) * pageSize;
-
-    const opt = as_result_async(this.repository.findMany);
-
-    const count = await this.tryCount();
-    if (count.is_err()) return Err(count.unwrap_err());
-
-    const result = (await opt({ ...arg, skip: offset, take: pageSize })).map_err(convertPrismaErrorToAppError);
-    if (result.is_err()) return Err(result.unwrap_err());
-
-    this.log = {
-      action: OperationAction.Read,
-      resourceIds: result.ok_or_throw().map(x => x.id),
-    };
-    return Ok([count.ok_or_throw(), result.ok_or_throw()]);
-  }
-
-  async tryFindUnique(...args: Parameters<typeof this.repository.findUnique>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.findUnique>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.findUnique);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    const _res = result.ok();
-    if (!_res) return Err(AppError.new(StatusCode.NotFound, `${this.resource} not found.`));
-
-    if (_res) {
-      this.log = {
-        action: OperationAction.Read,
-        resourceIds: [_res.id],
-      };
-    }
-    return result;
-  }
-
-  async tryFindFirst(...args: Parameters<typeof this.repository.findFirst>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.findFirst>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.findFirst);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    const _res = result.ok();
-    if (!_res) return Err(AppError.new(StatusCode.NotFound, `${this.resource} not found.`));
-
-    if (_res) {
-      this.log = {
-        action: OperationAction.Read,
-        resourceIds: [_res.id],
-      };
-    }
-    return result;
-  }
-
-  async tryCreate(...args: Parameters<typeof this.repository.create>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.create>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.create);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    this.log = {
-      action: OperationAction.Create,
-      resourceIds: [result.ok_or_throw().id],
-    };
-    return result;
-  }
-
-  async tryUpdate(...args: Parameters<typeof this.repository.update>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.update>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.update);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    this.log = {
-      action: OperationAction.Update,
-      resourceIds: [result.ok_or_throw().id],
-    };
-    return result;
-  }
-
-  async tryDelete(...args: Parameters<typeof this.repository.delete>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.delete>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.delete);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    this.log = {
-      action: OperationAction.Delete,
-      resourceIds: [result.ok_or_throw().id],
-    };
-    return result;
-  }
-
-  async tryDeleteMany(...args: Parameters<typeof this.repository.deleteMany>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.deleteMany>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.deleteMany);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    const _res = (arg?.where?.id as any)?.in;
-    if (Array.isArray(_res) && _res.length) {
-      this.log = {
-        action: OperationAction.Delete,
-        resourceIds: _res,
-      };
-    }
-    return result;
   }
 
   // Data create by uploading excel
--- server/src/services/region/index.ts
+++ server/src/services/region/index.ts
@@ -1,13 +1,13 @@
 import fs from "fs";
-import AppError, { StatusCode } from "../../utils/appError";
-import Result, { as_result_async, Err, Ok } from "../../utils/result";
+import AppError from "../../utils/appError";
+import Result, { as_result_async, Ok } from "../../utils/result";
 
 import { OperationAction, Resource } from "@prisma/client";
 import { CreateMultiRegionsInput } from "../../schemas/region.schema";
 import { convertPrismaErrorToAppError } from "../../utils/convertPrismaErrorToAppError";
 import { db } from "../../utils/db";
 import { parseExcel } from "../../utils/parseExcel";
-import { AppService, MetaAppService, Pagination } from "../type";
+import { AppService } from "../type";
 
 /**
  * RegionService class provides methods for managing region data.
@@ -15,11 +15,12 @@ import { AppService, MetaAppService, Pagination } from "../type";
  * @remarks
  * This class implements the AppService interface and is designed to handle operations related to regions.
  */
-export class RegionService extends MetaAppService implements AppService {
-  private repository = db.region;
-
+export class RegionService extends AppService<
+  typeof db.region
+> {
   constructor() {
-    super(Resource.Region, { action: OperationAction.Read, resourceIds: [] });
+    super(Resource.Region, { action: OperationAction.Read, resourceIds: [] }, db.region);
+    this.name = "Region";
   }
 
   /**
@@ -28,151 +29,6 @@ export class RegionService extends MetaAppService implements AppService {
    */
   static new() {
     return new RegionService();
-  }
-
-  async tryCount(): Promise<Result<number, AppError>> {
-    const opt = as_result_async(this.repository.count);
-
-    const result = (await opt()).map_err(convertPrismaErrorToAppError);
-
-    this.log = {
-      action: OperationAction.Read,
-      resourceIds: [],
-    };
-    return result;
-  }
-
-  async tryFindManyWithCount(
-    ...args: [{ pagination: Pagination; }, ...Parameters<typeof this.repository.findMany>]
-  ): Promise<
-    Result<[number, Awaited<ReturnType<typeof this.repository.findMany>>], AppError>
-  > {
-    const [{ pagination }, arg] = args;
-    const { page = 1, pageSize = 10 } = pagination;
-    const offset = (page - 1) * pageSize;
-
-    const opt = as_result_async(this.repository.findMany);
-
-    const count = await this.tryCount();
-    if (count.is_err()) return Err(count.unwrap_err());
-
-    const result = (await opt({ ...arg, skip: offset, take: pageSize })).map_err(convertPrismaErrorToAppError);
-    if (result.is_err()) return Err(result.unwrap_err());
-
-    this.log = {
-      action: OperationAction.Read,
-      resourceIds: result.ok_or_throw().map(x => x.id),
-    };
-    return Ok([count.ok_or_throw(), result.ok_or_throw()]);
-  }
-
-  async tryFindUnique(...args: Parameters<typeof this.repository.findUnique>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.findUnique>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.findUnique);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    const _res = result.ok();
-    if (!_res) return Err(AppError.new(StatusCode.NotFound, `${this.resource} not found.`));
-
-    if (_res) {
-      this.log = {
-        action: OperationAction.Read,
-        resourceIds: [_res.id],
-      };
-    }
-    return result;
-  }
-
-  async tryFindFirst(...args: Parameters<typeof this.repository.findFirst>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.findFirst>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.findFirst);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    const _res = result.ok();
-    if (!_res) return Err(AppError.new(StatusCode.NotFound, `${this.resource} not found.`));
-
-    if (_res) {
-      this.log = {
-        action: OperationAction.Read,
-        resourceIds: [_res.id],
-      };
-    }
-    return result;
-  }
-
-  async tryCreate(...args: Parameters<typeof this.repository.create>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.create>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.create);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    this.log = {
-      action: OperationAction.Create,
-      resourceIds: [result.ok_or_throw().id],
-    };
-    return result;
-  }
-
-  async tryUpdate(...args: Parameters<typeof this.repository.update>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.update>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.update);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    this.log = {
-      action: OperationAction.Update,
-      resourceIds: [result.ok_or_throw().id],
-    };
-    return result;
-  }
-
-  async tryDelete(...args: Parameters<typeof this.repository.delete>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.delete>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.delete);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    this.log = {
-      action: OperationAction.Delete,
-      resourceIds: [result.ok_or_throw().id],
-    };
-    return result;
-  }
-
-  async tryDeleteMany(...args: Parameters<typeof this.repository.deleteMany>): Promise<
-    Result<Awaited<ReturnType<typeof this.repository.deleteMany>>, AppError>
-  > {
-    const [arg] = args;
-
-    const opt = as_result_async(this.repository.deleteMany);
-
-    const result = (await opt(arg)).map_err(convertPrismaErrorToAppError);
-
-    const _res = (arg?.where?.id as any)?.in;
-    if (Array.isArray(_res) && _res.length) {
-      this.log = {
-        action: OperationAction.Delete,
-        resourceIds: _res,
-      };
-    }
-    return result;
   }
 
   // Data create by uploading excel
